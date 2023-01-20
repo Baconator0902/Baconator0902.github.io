8bc198683bec39e631493ca3c183ac5bc8f63308
Ethan Maraldo <ethanmaraldo@gmail.com>
2023-01-19 19:43:57 -0800

    AYO

---
+++ InAccount.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="screen and (max-width: 1200px)" href="css/phone.css">
+    <link rel="stylesheet" media="screen and (min-width: 1200px)" href="css/main.css">
+    <script src="js/navbar.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Teko:wght@400;600&display=swap" rel="stylesheet"> 
+    <script src="js/signUpLogin.js"></script>
+    <title>login/signup</title>
+</head>
+<body>
+</body>
+</html>
