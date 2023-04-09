d69835d4b728a781952f90b506dde0650135f7f3
emaraldo1 <ethanmaraldo@gmail.com>
2023-04-08 17:29:22 -0700

    asd

---
+++ src/bookingsandSuch.js
@@ -0,0 +1,20 @@
+import{auth} from '/src/firebaseInit.js';
+import{ onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js';
+
+var bookButton =  document.getElementById("bookSubmit");
+console.log(bookButton);
+var trainingButton =  document.getElementById("trainingSubmit");
+console.log(trainingButton);
+var physicalButton =  document.getElementById("physicalSubmit");
+console.log(physicalButton);
+
+
+function onClickBookATrip(){
+
+}
+function onClickBookATraining(){
+
+}
+function onClickBookAPhysical(){
+    
+}
