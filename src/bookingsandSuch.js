be39a33667df0bc157edc622a5d11e6facbba6a2
emaraldo1 <ethanmaraldo@gmail.com>
2023-04-29 11:01:06 -0700

    a

---
+++ src/bookingsandSuch.js
@@ -0,0 +1,72 @@
+
+import{auth, database} from '/src/firebaseInit.js';
+import{ onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js';
+import { set, ref, child, push, update } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+var bookButton =  document.getElementById("bookSubmit");
+var trainingButton =  document.getElementById("trainingSubmit");
+var physicalButton =  document.getElementById("physicalSubmit");
+
+
+//Commit For GITTTT
+function onClickBookATrip(){
+
+      const user = auth.currentUser;
+      if (user) {
+          var startDate = document.getElementById("date1");
+          var partySize = document.getElementById("party_size")
+          var uid = user.uid;
+        
+          update(ref(database, 'users/' + uid),{
+           tripDateBooked : startDate.value,  
+           partySizeBooked : partySize.value,
+        })
+        .then(()=>{window.location.replace("./index.html");}, err => {console.error(err)});
+  
+        } else {
+            window.alert("Trip succesfully planned. To save a booking, please create an account and resubmit");
+        }
+    
+
+}
+function onClickBookATraining(){
+
+    const user = auth.currentUser;
+    if (user) {
+        var startDateT = document.getElementById("dateTraining");
+        var partySizeT = document.getElementById("party_sizeTraining")
+        var uid = user.uid;
+     
+       
+        update(ref(database, 'users/' + uid),{
+         trainingBooked : startDateT.value,  
+         partySizeTrainingBooked : partySizeT.value,
+      })
+      .then(()=>{window.location.replace("./index.html");}, err => {console.error(err)});
+
+      } else {
+          window.alert("Training succesfully planned. To save a training, please create an account and resubmit");
+      }
+  
+}
+function onClickBookAPhysical(){
+    const user = auth.currentUser;
+    if (user) {
+        var startDateP = document.getElementById("physicalDate");
+        var partySizeP = document.getElementById("party_sizePhysical")
+        var uid = user.uid;
+     
+        
+        update(ref(database, 'users/' + uid),{
+         physicalBooked : startDateP.value,  
+         partySizePhysicalBooked : partySizeP.value,
+      })
+      .then(()=>{window.location.replace("./index.html");}, err => {console.error(err)});
+
+      } else {
+          window.alert("Physical succesfully planned. To save this planning, please create an account and resubmit");
+      }
+}
+
+bookButton.addEventListener('click', onClickBookATrip);
+trainingButton.addEventListener('click', onClickBookATraining);
+physicalButton.addEventListener('click', onClickBookAPhysical);
---
+++ src/navbarController.js
@@ -0,0 +1,21 @@
+import{ onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js';
+import{auth} from '/src/firebaseInit.js';
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+
+      var signInButton = $("#navBar").children().children().last().children();
+          $(signInButton).text("Profile");
+          $(signInButton).attr("href", "profile.html");
+     
+
+
+    } else {
+    var signInButton = $("#navBar").children().children().last().children();
+      $(signInButton).text("Sign up/Log in");
+        $(signInButton).attr("href", "signUpLogin.html");
+    }
+});
+//I wanna keep this too
