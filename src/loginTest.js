aa461a142c8645442ae8382fa88109a1d468021f
emaraldo1 <ethanmaraldo@gmail.com>
2023-04-30 12:41:23 -0700

    asd

--- src/loginTest.js
+++ src/loginTest.js
@@ -5,8 +5,7 @@ import{signInWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/9.17
 
     var loginButton =  document.getElementById("loginButton");
     console.log(loginButton);
-    var auth2 = auth;
-
+    
 
 function onClickFunction2() {
 
@@ -14,13 +13,13 @@ function onClickFunction2() {
     
     var password = document.getElementById("password3").value;
     console.log(email);
-    console.log(auth2);
-   signInWithEmailAndPassword(auth2, email, password)
+    console.log(auth);
+   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
 // Signed in 
     var user = userCredential.user;
     console.log(user);
-   // window.location.replace("http://127.0.0.1:5500/index.html");
+   window.location.replace("./index.html");
 // ...
  })
 .catch((error) => {
