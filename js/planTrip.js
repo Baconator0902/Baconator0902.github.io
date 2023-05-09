fbcaa05923c9a6aa4272f1d04ed239ad8791d135
Bac0nator0902 <lucas123famous@gmail.com>
2023-05-09 15:14:21 -0400

    adsf

--- js/planTrip.js
+++ js/planTrip.js
@@ -1,12 +1,5 @@
 function planTripFormSubmit() {
-    locations = document.getElementsByName('launch_location');
-    for(i = 0; i < locations.length; i++) {
-        if(locations[i].checked) {
-            checkedLocation = locations[i];
-            locationNumber = i;
-            console.log(checkedLocation);
-        }
-    }
+    newLocation = document.getElementById('launch_location').value;
 
     partySize = document.getElementById("party_size").value;
     length = document.getElementById("trip_length").value;
@@ -15,7 +8,7 @@ function planTripFormSubmit() {
     monthDays = date.charAt(6);
     month = monthTens + monthDays;
 
-    cost = ((1 + (1 / (locationNumber + 3))) * 200) * ((partySize + 10) / 15) * (length * 200) * 0.2;
+    cost = ((((newLocation))) / 100) * ((partySize + 10) / 15) * (length * 200) * 0.015;
     if (month > 5 && month < 9) {
         cost *= 1.2;
     } else if (month < 3 || month > 11) {
